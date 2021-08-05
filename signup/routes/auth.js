ef1ace0da8a4462815f332359e9e0a99fea2fa6c
Fahadulhassan1 <fahad.khalid01234@gmail.com>
2021-08-05 15:23:34 +0500

    users

--- signup/routes/auth.js
+++ signup/routes/auth.js
@@ -9,4 +9,13 @@ const { signup } = require("../controllers/auth");
 router.post("/signup", signup);
 router.post("/email-activate", activateAccount);
 
+router.get('/signup' ,  async (req, res)=>{
+  try{
+      const users = await User.find({})
+      res.send(users);
+  }catch(e){
+      res.status(500).send(e)
+  }
+})
+
 module.exports = router;
