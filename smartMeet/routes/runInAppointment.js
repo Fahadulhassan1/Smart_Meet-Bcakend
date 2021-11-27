927a795528be9574a539f57074a2c4278a9f1bb7
Fahadulhassan1 <fahad.khalid01234@gmail.com>
2021-11-27 17:23:24 +0500

    added changes

--- smartMeet/routes/runInAppointment.js
+++ smartMeet/routes/runInAppointment.js
@@ -9,11 +9,11 @@ const router = express.Router();
 
 router.post("/visitor/runInappointment", async (req, res, next) => {
   // const ts = ['10:00-10:30','10:30-11:00','11:00-11:30','11:30-12:00','12:00-12:30','12:30-13:00','13:00-13:30','13:30-14:00','14:00-14:30','14:30-15:00','15:00-15:30','15:30-16:00'];
-  console.log("done");
+
   try {
     var {
       employeeId,
-   
+
       visitorName,
       visitorEmail,
       visitorPhone,
@@ -24,15 +24,7 @@ router.post("/visitor/runInappointment", async (req, res, next) => {
       avatar,
     } = req.body;
 
-  
     
-    
-
-    if (appointments.length > 0 && appointments[0].isRejected) {
-      return res.send({
-        error: "already Appointment slot booked  on this time slot for this host ",
-      });
-    } else {
       let AppointmentRequest = new RunInAppointment({
         employeeId,
         visitorName,
@@ -50,20 +42,22 @@ router.post("/visitor/runInappointment", async (req, res, next) => {
         }
         return res.json({ message: "appointment request sent successfully" });
       });
-    }
+    
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error });
   }
 });
 
-router.get('/visitor/runInappointment',async (req, res) => {
-   try {
-     const appointements = await RunInAppointment.find({}).populate('employeeId');
-     res.send(appointements);
-   } catch (e) {
-     res.status(500).send(e);
-   }
-})
+router.get("/visitor/runInappointment", async (req, res) => {
+  try {
+    const appointements = await RunInAppointment.find({}).populate(
+      "employeeId"
+    );
+    res.send(appointements);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
 
 module.exports = router;
